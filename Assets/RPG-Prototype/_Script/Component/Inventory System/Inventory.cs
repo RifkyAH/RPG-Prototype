ba97a343e6e72d4d47488770e6ee8f80f787fb17
RifkyAH <rifkyabdillah223@gmail.com>
2025-08-12 02:23:17 +0700

    Add_ItemPickup & Integrate AddItem function into Inventory System
    Added ItemPickup script to handle player interactions with items in the game world.
    Integrated AddItem function into the Inventory System to store picked-up items.
    This allows items to be automatically added to the player's inventory upon collection.

--- Assets/RPG-Prototype/_Script/Component/Inventory System/Inventory.cs
+++ Assets/RPG-Prototype/_Script/Component/Inventory System/Inventory.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public List<Item> ItemCollection = new List<Item>();
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddItem(Item item)
     {
-        
+        ItemCollection.Add(item);
     }
+
 }
